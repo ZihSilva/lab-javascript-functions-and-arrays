eddd14e7fabdfddef6f9c46fb54da04e62dc142f
Zimarlen Silva <zimarlensilva@gmail.com>
2022-08-11 21:29:57 -0700

    first part solved

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -28,12 +28,20 @@ return large
 }
 
 
-
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers (){}
 
+function sumNumbers(numbers) {
+  
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum;
+
+}
 
 
 // Iteration #3.1 Bonus:
